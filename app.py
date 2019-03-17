8fd8d636fc9f0ae050abc9a6a397c29809b6516a
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2019-03-16 20:34:00 -0400

    new

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from bot import mainFunction
 from flask import Flask
-my_awesome_app = Flask(__name__)
+app = Flask(__name__)
 
 
-@my_awesome_app.route('/')
+@app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Go to /api/rap/Insert String that you want raps to '
 
 
-@my_awesome_app.route("/api/rap/<phrase>")
+@app.route("/api/rap/<phrase>")
 def hello(phrase):
     return mainFunction(phrase)
 
 
 if __name__ == '__main__':
-    my_awesome_app.run()
+    app.run()
